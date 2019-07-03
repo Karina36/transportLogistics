3b5ba0851a4fb238e4324819cac626553e412b6c
Karina36 <karina.36@mail.ru>
2019-07-03 08:19:27 +0500

    end

--- TransportLogistics/Main.cs
+++ TransportLogistics/Main.cs
@@ -264,6 +264,8 @@ namespace TransportLogistics
         }
         private void calc()
         {
+            int truckW = Convert.ToInt32(truck[1]); int truckH = Convert.ToInt32(truck[2]); int truckL = Convert.ToInt32(truck[3]);
+            //Первая партия
             int width = Convert.ToInt32(truck[1]) / Convert.ToInt32(cargoData.Rows[0][4]); int length = Convert.ToInt32(truck[3]) / Convert.ToInt32(cargoData.Rows[0][5]);
             int height = Convert.ToInt32(truck[2]) / Convert.ToInt32(cargoData.Rows[0][3]);
             int[] wlh1 = new int[4] { width, length, height, width*length*height };
@@ -286,6 +288,24 @@ namespace TransportLogistics
             height = Convert.ToInt32(truck[2]) / Convert.ToInt32(cargoData.Rows[0][5]);
             int[] hwl1 = new int[4] { width, length, height, width * length * height };
 
-        } 
+            int[] max1 = new int[6] { wlh1[3], hlw1[3], whl1[3],  lwh1[3], lhw1[3], hwl1[3]};
+
+            if (Convert.ToInt32(dt.Rows[0].ItemArray[1]) > max1.Max()) dt.Rows[0].SetField(1, max1.Max());
+
+            //Свободное пространство
+            int[] widthF = new int[6] { truckW - wlh1[0], truckW - hlw1[0], truckW - whl1[0], truckW - lwh1[0], truckW - lhw1[0], truckW - hwl1[0] };
+            int[] lengthF = new int[6] { truckL - wlh1[1], truckL - hlw1[1], truckL - whl1[1], truckL - lwh1[1], truckL - lhw1[1], truckL - hwl1[1] };
+            int[] heightF = new int[6] { truckH - wlh1[2], truckH - hlw1[2], truckH - whl1[2], truckH - lwh1[2], truckH - lhw1[2], truckH - hwl1[2] };
+
+            //Свободный объем
+            int[] widthV = new int[6] { widthF[0] * truckL* truckH, widthF[1] * truckL * truckH, widthF[2] * truckL * truckH, widthF[3] * truckL * truckH, widthF[4] * truckL * truckH, widthF[5] * truckL * truckH };
+            int[] lengthV = new int[6] { lengthF[0] * (truckL - heightF[0]) * (truckW- widthF[0]), lengthF[1] * (truckL - heightF[1]) * (truckW - widthF[1]), lengthF[2] * (truckL - heightF[2]) * (truckW - widthF[2]),
+                 lengthF[3] * (truckL - heightF[3]) * (truckW- widthF[3]),  lengthF[4] * (truckL - heightF[4]) * (truckW- widthF[4]),  lengthF[5] * (truckL - heightF[5]) * (truckW- widthF[5])};
+            int[] heightV = new int[6] { lengthF[0]* (truckW - widthF[0]) * truckH, lengthF[1] * (truckW - widthF[1]) * truckH, lengthF[2] * (truckW - widthF[2]) * truckH,
+                lengthF[3]* (truckW - widthF[3]) * truckH, lengthF[4]* (truckW - widthF[4]) * truckH, lengthF[5]* (truckW - widthF[5]) * truckH};
+            int[] sumV = new int[6] { widthV[0] + lengthV[0] + heightV[0], widthV[1] + lengthV[1] + heightV[1], widthV[2] + lengthV[2] + heightV[2], widthV[3] + lengthV[3] + heightV[3], widthV[4] + lengthV[4] + heightV[4],
+            widthV[5] + lengthV[5] + heightV[5]};
+
+        }
     }
 }
